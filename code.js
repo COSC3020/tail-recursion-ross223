1aa210d33be220314fdcd33bd3f32d4e03eb1c6f
ross223 <144744308+ross223@users.noreply.github.com>
2024-04-20 20:04:10 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
+function myFib(num){
+    var fib1 = 0;
+    var fib2 = 1;
+    return tailFib(num, fib1, fib2);
+}
 
+function tailFib(num, fib1, fib2){
+    if (num == 1) {
+        return fib2;
+    }
+    if (num == 0) {
+        return fib1;
+    }
+    return tailFib(num - 1, fib2, fib1 + fib2);
+}
+
+module.exports = {myFib, tailFib};
