a240401c37674cdf60250e115b77b43a95fb6540
ross223 <144744308+ross223@users.noreply.github.com>
2024-04-20 20:03:58 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,1 +1,17 @@
+const assert = require("assert");
+const code = require("./code.js");
+
+function fib(n) {
+    if (n == 1) {
+        return 1;
+    }
+    if (n == 0) {
+        return 0;
+    }
+    return fib(n-1) + fib(n-2);
+}
+
+randint = Math.floor(Math.random() * 100);
+
+assert(code.myFib(randint) == fib(randint))
 
