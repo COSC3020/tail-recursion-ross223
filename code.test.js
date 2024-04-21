37fdc5da37b81d51457a930f58bed26c3dee9fab
ross223 <144744308+ross223@users.noreply.github.com>
2024-04-20 20:29:04 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,4 +1,4 @@
-/*const assert = require("assert");
+const assert = require("assert");
 const code = require("./code.js");
 
 function fib(n) {
@@ -11,45 +11,8 @@ function fib(n) {
     return fib(n-1) + fib(n-2);
 }
 
-randint = Math.floor(Math.random() * 100);
+var randint = Math.floor(Math.random() * 100);
 
 assert(code.myFib(randint) == fib(randint))
-*/
 
-const code = require("./code.js");
-const assert = require("assert");
-
-
-function fib(n) { //recursive fib function taken from the lecture slides
-  if (n == 0)
-    return 0;
-  if(n <= 2) 
-    return 1;
-  else 
-    return fib(n-1) + fib(n-2);
-}
-
-///////////Test 1 
-let n = 0; //base case 
-assert(fib(n) == code.myFib(n));
-
-
-///////////Test 2 
-n = 1; //base case 
-assert(fib(n) == code.myFib(n));
-
-
-///////////Test 3 
-n = 6; 
-assert(fib(n) == code.myFib(n));
-
-
-///////////Test 3 
-n = 6; 
-assert(fib(n) == code.myFib(n));
-
-
-///////////Test 4
-n = 15; 
-assert(fib(n) == code.myFib(n));
 
