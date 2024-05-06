d26fdde2c812506a0e3917ceb17e8821d18d3e71
ross223 <144744308+ross223@users.noreply.github.com>
2024-05-06 15:25:02 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -11,7 +11,7 @@ function fib(n) {
     return fib(n-1) + fib(n-2);
 }
 
-var randint = Math.floor(Math.random() * 10);
+var randint = Math.floor(Math.random() * 10);  // This website was helpful for Math.random https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 assert(code.myFib(randint) == fib(randint))
 
